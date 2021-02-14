a4b46d6a03e4643eb2017482b64a3fc5cd754424
Abu Shaid Sabbir <75726900+sabbir102629@users.noreply.github.com>
2021-02-14 09:54:41 -0800

    feb14.2

---
+++ extra/practice.js
@@ -0,0 +1,20 @@
+let js = 'awesome';
+console.log(js);
+console.log(17-31+20*2);
+let firstName ='Abu shaid ';
+let lastName = 'Sabbir';
+console.log(firstName + ' ' + lastName + 'is a student of computer techlnology.'
++ ' '+ lastName + ' is my nickname');
+ let x = 'abushaid';
+ console.log(typeof(x));
+ let country = 'Bangladesh';
+ let continent = 'asia';
+ let population = 'twenty core';
+ console.log(country + ' ' + continent +' ' + population);
+ let isIsland = true;
+ let language;
+
+ console.log(typeof(isIsland));
+ console.log(typeof(population));
+ console.log(typeof(country));
+ console.log(typeof(language));
